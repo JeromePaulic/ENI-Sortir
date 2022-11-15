b2e8dcef5204800b06e22923ef0d568121566a29
elodi <elodie.arevalo2022@campus-eni.fr>
2022-11-15 17:19:48 +0100

    les entitées et leur repository

---
+++ src/Controller/SortieController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Sortie;
+use App\Form\SortieType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SortieController extends AbstractController
+{
+    #[Route('/sortie/cree', name: 'sortie_cree')]
+    public function cree(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $sortie=new Sortie();
+        $sortieForm = $this-> createForm(SortieType::class,$sortie);
+
+        $sortieForm->handleRequest($request);
+            if($sortieForm->isSubmitted() && $sortieForm->isValid()){
+                $entityManager->persist($sortie);
+                $entityManager->flush();
+            }
+
+        return $this->render('sortie/cree.html.twig', [
+            'sortieForm'=> $sortieForm->createView()
+        ]);
+    }
+}
